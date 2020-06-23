50a1e28e941b7e7697fe9edb6aa4f5d9dc55c24c
KiranMorais <67342531+KiranMorais@users.noreply.github.com>
2020-06-24 03:03:17 +0530

    initial xcode project

---
+++ YouTube-Oneday-Player/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YouTube-Oneday-Player
+//
+//  Created by Kiran Morais on 24/06/20.
+//  Copyright © 2020 Heartinz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
