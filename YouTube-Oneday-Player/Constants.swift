998a912778264c790bc82210c8dd54febcb56739
KiranMorais <67342531+KiranMorais@users.noreply.github.com>
2020-06-26 00:45:17 +0530

    Video Api Call

---
+++ YouTube-Oneday-Player/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  YouTube-Oneday-Player
+//
+//  Created by Kiran Morais on 25/06/20.
+//  Copyright © 2020 Heartinz. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static var API_KEY = "AIzaSyCzrL75PiqkPInLrQmPM4rNcF7_cbRVTfQ"
+    static var PLAYLIST_ID = "PL9bw4S5ePsEGISygM8mrvI5JULlmgCeds"
+    static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.PLAYLIST_ID)"
+}
+
---
+++ YouTube-Oneday-Player/Video.swift
@@ -0,0 +1,22 @@
+//
+//  Video.swift
+//  YouTube-Oneday-Player
+//
+//  Created by Kiran Morais on 25/06/20.
+//  Copyright © 2020 Heartinz. All rights reserved.
+//
+
+import Foundation
+
+struct Video {
+    var videoId = ""
+    var titile = ""
+    var description = ""
+    var thumbnail = ""
+    var published = ""
+    
+    
+    
+    
+    
+}
